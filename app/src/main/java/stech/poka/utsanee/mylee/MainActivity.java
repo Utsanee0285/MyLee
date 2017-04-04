827f9facbd9209feb1177484dd584ee1f6deef8f
utsanee <utsanee@stech.ac.th>
2017-04-04 16:02:18 +0700

    New...Afternoon

--- app/src/main/java/stech/poka/utsanee/mylee/MainActivity.java
+++ app/src/main/java/stech/poka/utsanee/mylee/MainActivity.java
@@ -2,12 +2,35 @@ package stech.poka.utsanee.mylee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Explicit เพื่อประกาศตัวแปร
+    // Access(public private)  Type(int str dub bul)  Name
+    private EditText userEditText,passwordEditText;
+    private TextView textView;
+    private Button button;
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Initial View คือการผูกตัวแปร กับ View ที่อยู่ Activity
+        intialView();
+
+
+    } //Main Method
+
+    private void intialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }   //Main Class นี่คิอ คลาสหลัก
